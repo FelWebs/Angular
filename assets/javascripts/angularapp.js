44cccce0827e7affb1ec352d6bed13ed67792442
Mikkel Alstrup Olsen <136491@RTS-365.dk>
2016-11-23 12:57:43 +0100

    Rev 7 - med IMG

--- assets/javascripts/angularapp.js
+++ assets/javascripts/angularapp.js
@@ -1,6 +1,6 @@
 /*globals angular*/
 
-var app = angular.module('myApp', ['ui.router']);
+var app = angular.module('myApp', ['ui.router', 'ngSanitize']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
     "use strict";
@@ -10,11 +10,27 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('forside', {
             url: '/forside',
-            templateUrl: './assets/views/partial-forside.html'
+            templateUrl: './assets/views/partial-forside.html',
+            controller: 'forside'
         })
 
         .state('kontakt', {
             url: '/kontakt',
             templateUrl: './assets/views/partial-kontakt.html'
+        });
+});
+
+app.controller("forside", function ($scope, $http) {
+    "use strict";
+
+    $http.get("./assets/javascripts/model.json")
+        .then(function (response) {
+            var i,
+                pages = response.data;
+            for (i = 0; i < pages.length; i = i + 1) {
+                if (pages[i].title === "Forside") {
+                    $scope.page = pages[i];
+                }
+            }
         });
 });
