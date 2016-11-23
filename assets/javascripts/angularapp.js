6a8fd56a3525595caa57f0edfee9b0ad0fb00648
Mikkel Alstrup Olsen <136491@RTS-365.dk>
2016-11-23 10:33:19 +0100

    Rev 4

--- assets/javascripts/angularapp.js
+++ assets/javascripts/angularapp.js
@@ -1,11 +1,28 @@
 /*globals angular*/
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', [ui.router]);
 
 app.controller('myCtrl', function ($scope, $http) {
     "use strict";
     $http.get("./assets/javascripts/model.json")
         .then(function(response) {
             $scope.names = response.data;
+        });
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    "use strict";
+
+    $urlRouterProvider.otherwise('/forside');
+
+    $stateProvider
+        .state('forside', {
+            url: '/forside',
+            templateUrl: '.assets/views/partial-forside.html'
+        })
+
+        .state('kontakt', {
+            url: '/kontakt',
+            templateUrl: './assets/views/partial-kontakt.html'
         });
 });
